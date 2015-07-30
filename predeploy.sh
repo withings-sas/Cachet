9f090548bf9408ca87e7c3638f80af77c1ce5af1
Etienne Adam <etienne.adam@withings.com>
2015-07-30 14:12:23 +0200

    disable cache

--- predeploy.sh
+++ predeploy.sh
@@ -16,11 +16,12 @@ rsync -az --delete --exclude="vendor/" --exclude="composer.phar" ./ $BUILD_USER@
 # Build
 ssh $BUILD_USER@$BUILD_SERVER "cd $BUILD_PATH; [[ -f composer.phar ]] || curl -sS https://getcomposer.org/installer | php"
 ssh $BUILD_USER@$BUILD_SERVER "cd $BUILD_PATH; ./composer.phar install --no-dev -o"
+ssh $BUILD_USER@$BUILD_SERVER "cd $BUILD_PATH; ./composer.phar update --no-dev"
 
 # Retrieve build
-if [ -d build ]; then
-  rm -rf build
-fi
-mkdir build
+#if [ -d build ]; then
+#  rm -rf build
+#fi
+#mkdir build
 rsync -az --delete $BUILD_USER@$BUILD_SERVER:$BUILD_PATH/vendor/ vendor/
 
